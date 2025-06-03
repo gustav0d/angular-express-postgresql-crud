482a8c477c11681a21fd409a9a618f0214b1dbb2
Gustavo Dantas <gusdantas15@gmail.com>
2025-06-03 11:09:23 -0300

    feat(error): add errorHandler function to specify the way errors are returned

---
+++ apps/backend/src/modules/error/utils/errorResponse.ts
@@ -0,0 +1,17 @@
+import type { Response } from 'express';
+
+type ErrorHandlerParams = {
+  errorTitle: string;
+  errorMessage: string;
+  statusCode?: number;
+};
+
+export const errorHandler = (
+  response: Response,
+  { errorTitle, errorMessage, statusCode = 400 }: ErrorHandlerParams,
+) => {
+  return response.status(statusCode).json({
+    error: errorTitle,
+    message: errorMessage,
+  });
+};
