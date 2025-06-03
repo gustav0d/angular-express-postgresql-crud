596da8a8c1097b50194861bf4864d01ebd4c8d2f
Gustavo Dantas <gusdantas15@gmail.com>
2025-06-03 14:46:55 -0300

    feat(login): add loginService

---
+++ apps/backend/src/modules/auth/services/loginService.ts
@@ -0,0 +1,40 @@
+import { z } from 'zod';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+import { User, type UserAttributes } from '../userModel.ts';
+import { config } from '../../../config.ts';
+
+const loginSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z.string().min(1, { message: 'Password is required' }),
+});
+
+type LoginInput = z.infer<typeof loginSchema>;
+
+export async function loginUserService(loginData: LoginInput) {
+  const validatedData = loginSchema.parse(loginData);
+
+  const userExists = await User.scope('withPassword').findOne({
+    where: { email: validatedData.email },
+  });
+
+  if (!userExists) {
+    throw new Error('Invalid email or password');
+  }
+
+  const user = userExists.dataValues as Required<UserAttributes>;
+
+  const isPasswordValid = bcrypt.compare(validatedData.password, user.password);
+  if (!isPasswordValid) {
+    throw new Error('Invalid email or password');
+  }
+
+  const token = jwt.sign({ userId: user.id }, config.JWT_SECRET);
+
+  const { password, ...userWithoutPassword } = user;
+  return {
+    user: userWithoutPassword,
+    token,
+  };
+}
