1df7a6309434d193563aa3dd32f6414f883c1cc3
Gustavo Dantas <gusdantas15@gmail.com>
2025-06-08 08:53:24 -0300

    feat(frontend/auth): add register page

--- apps/frontend/src/app/features/auth/register/register.component.ts
+++ apps/frontend/src/app/features/auth/register/register.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink, RouterModule],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    RouterLink,
+    RouterModule,
+  ],
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {}
